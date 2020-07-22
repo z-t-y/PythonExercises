a3ebf85f56d0379014833917da422ef9985f79d5
AndyZhou <AndyForever0108@outlook.com>
2020-07-22 11:50:23 +0800

    完成增血功能

---
+++ README.md
@@ -0,0 +1,3 @@
+# 一个小型命令行RPG游戏
+由[GreyLi/PythonExercises#6](https://github.com/greyli/PythonExercises#6-%E6%96%87%E5%AD%97%E5%86%92%E9%99%A9%E6%B8%B8%E6%88%8F)得到灵感
+
--- simple_rpg_game.py
+++ simple_rpg_game.py
@@ -1,22 +1,26 @@
-from random import randint
+from random import randint, random
 from sys import exit, version
 
 
 class Player(object):
     def __init__(self, name):
         self.name = name
-        self.blood_vol = randint(900, 2000)
+        self.hp = randint(900, 2000)
         self.dead = False
 
     def be_attacked(self):
         decrease = randint(50, 200)
-        if randint(1, 10) == randint(1, 10):
-            crit = True
+        if self.lucky():  # hiahiahia :P
             print("暴击")
             decrease = randint(500, 1000)
-        self.blood_vol -= decrease
-        if self.blood_vol <= 0:
+        self.hp -= decrease
+        if self.hp <= 0:
             self.dead = True
+
+    def lucky(self):
+        if randint(1, 10) == randint(1, 10):
+            return True
+        return False
 
 
 print("Simple Command Line RPG Game By Andy")
@@ -39,24 +43,32 @@ for i in range(num_of_players):
     )
 
 
+def describe_players():
+    for player in players:
+        if not player.dead:
+            print(f"玩家{player.name}血量为{player.hp}")
+
+
 def mainloop():
-    cnt = num_of_players
+    counter = num_of_players
     while True:
-        for player in players:
-            if not player.dead:
-                print(f"玩家{player.name}血量为{player.blood_vol}")
+        describe_players()
+        # 新一轮攻击开始
         for player in players:
             if not player.dead:
                 attack_index = input(f"{player.name}请输入攻击对象的序号：")
+                # 检查用户输入是否为整数
                 if attack_index.isdigit():
                     attack_index = int(attack_index)
                     pass
                 else:
                     print("不符合攻击规范")
                     continue
+                # 检查自残情况
                 if attack_index == players.index(player) + 1:
                     print("不能自残")
                     continue
+                # 检查攻击死亡玩家
                 attacked_player = players[attack_index - 1]
                 if attacked_player.dead:
                     print("不能攻击已死亡的玩家")
@@ -64,12 +76,25 @@ def mainloop():
                 attacked_player.be_attacked()
                 if attacked_player.dead:
                     print(f"玩家{attacked_player.name}死亡")
-                    cnt -= 1
+                    counter -= 1  # 将存活数量 - 1
                 else:
-                    print(f"{attacked_player.name}血量为{attacked_player.blood_vol}")
-            if cnt == 1 and not player.dead:
+                    print(f"{attacked_player.name}血量为{attacked_player.hp}")
+            if counter == 1 and not player.dead:
                 print(f"{player.name}获胜")
+                input("按下<Enter>键结束游戏")
                 exit(0)
+        # 中场休息 增加hp
+        print('补充能量'.center(20, '-'))
+
+        for player in players:
+            if not player.dead:
+                eat_increase = randint(20, 40)
+                if player.lucky():
+                    eat_increase = randint(500, 1000)
+                    print(f"{player.name}运气爆棚，", end='')
+                player.hp += eat_increase
+                print(f"玩家{player.name}增加血量{eat_increase}")
+        print('补充能量完毕'.center(20, '-'))
 
 
 if __name__ == "__main__":
